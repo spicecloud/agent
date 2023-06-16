a1229c9cdec4b237a9af5a4ce73ca2a9463aaeb1
Ankush Patel <ankush@Ankushs-MacBook-Pro.local>
2023-06-15 23:09:31 -0700

    threaded status details callback

--- spice/training/actions.py
+++ spice/training/actions.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 import logging
 import os
+import threading
 from typing import Dict, Optional
 
 from aiohttp import client_exceptions
@@ -43,6 +44,93 @@ MODEL_BUCKET_NAME = "spice-models"
 TOKENIZER_MAX_LENGTH = 32
 
 
+class ThreadedStatusDetailsCallbackDecorator:
+    """
+    A class decorator that enables threaded functionality for StatusDetailsCallback
+    """
+
+    def __init__(self, is_threaded: Optional[bool] = False):
+        self.is_threaded = is_threaded
+        self.current_thread = None
+        self.wrapped_class = "StatusDetailsCallback"
+
+    def __call__(self, cls):
+        if not self.is_threaded:
+            return cls
+
+        # TODO: Remove cls.__name__, import the target class, and check with
+        # cls instance type
+        if cls.__name__ != self.wrapped_class:
+            raise TypeError(
+                f"{self.__class__.__name__} can only be applied to {self.wrapped_class}"
+            )
+
+        # Wrap functions with defined decorators
+        # Example: on_step_end gets wrapped with on_step_end_decorator
+        decorated_methods = []
+        for name, value in cls.__dict__.items():
+            if callable(value) and name != "__init__":
+                decorated_method = getattr(self, f"{name}_decorator", None)
+                if decorated_method:
+                    decorated_method = decorated_method(value)
+                    setattr(cls, name, decorated_method)
+                    decorated_methods.append(name)
+
+        # Set wrapped class' attributes
+        setattr(cls, "decorated_methods", decorated_methods)
+        return cls
+
+    def on_train_begin_decorator(self, function):
+        def wrapper(*args, **kwargs):
+            if not self.current_thread:
+                on_train_begin_thread = self._get_threaded_function(
+                    function, *args, **kwargs
+                )
+                self.current_thread = on_train_begin_thread
+                on_train_begin_thread.start()
+
+        return wrapper
+
+    def on_step_end_decorator(self, function):
+        def wrapper(*args, **kwargs):
+            # If the current thread is no longer alive, we join it with the main thread
+            # and reset current thread
+            if self.current_thread and not self.current_thread.is_alive():
+                self.current_thread.join()
+                del self.current_thread
+                self.current_thread = None
+
+            # We set the current thread and start it's activity
+            if not self.current_thread:
+                step_end_thread = self._get_threaded_function(function, *args, **kwargs)
+                self.current_thread = step_end_thread
+                self.current_thread.start()
+
+        return wrapper
+
+    def on_train_end_decorator(self, function):
+        def wrapper(*args, **kwargs):
+            # Complete final on_step_end update
+            if self.current_thread and self.current_thread.is_alive():
+                self.current_thread.join()
+                del self.current_thread
+                self.current_thread = None
+
+            # We execute on_train_end on the main thread since it is the final call
+            # in StatusDetailsCallback and we have to wait for it's completion.
+            function(*args, **kwargs)
+
+        return wrapper
+
+    def _get_threaded_function(self, function, *args, **kwargs) -> threading.Thread:
+        def target_function():
+            function(*args, **kwargs)
+
+        thread = threading.Thread(target=target_function)
+        return thread
+
+
+@ThreadedStatusDetailsCallbackDecorator(is_threaded=True)
 class StatusDetailsCallback(transformers.TrainerCallback):
     """
     A [`TrainerCallback`] that sends the progress of training or evaluation
@@ -106,7 +194,7 @@ class StatusDetailsCallback(transformers.TrainerCallback):
                 self.training._update_training_round(
                     training_round_id=self.training_round_id,
                     status=self.status,
-                    status_details={"progress": 0},
+                    status_details={"progress": 100},
                 )
 
 
