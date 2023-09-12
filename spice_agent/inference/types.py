5190c9fed9f8cadbe8988e79f8474558ca4659ba
Ankush Patel <patelankush.2012@gmail.com>
2023-09-11 19:03:24 -0700

    update types.py

--- spice_agent/inference/types.py
+++ spice_agent/inference/types.py
@@ -6,20 +6,34 @@ from dataclasses import dataclass
 
 from diffusers import (
     StableDiffusionPipeline,
+    StableDiffusionImg2ImgPipeline,
     StableDiffusionXLPipeline,
     StableDiffusionXLImg2ImgPipeline,
+    StableDiffusionXLControlNetPipeline,
 )
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
 from diffusers.pipelines.stable_diffusion_xl import StableDiffusionXLPipelineOutput
 
-# TODO: Add user options data type
+SUPPORTED_MODEL_REPOSITORY_IDS: List[str] = [
+    "stabilityai/stable-diffusion-2-1-base",
+    "stabilityai/stable-diffusion-xl-base-1.0",
+]
 
 # Supported stable diffusion pipelines
 TStableDiffusionPipeline = TypeVar(
     "TStableDiffusionPipeline",
     StableDiffusionPipeline,
+    StableDiffusionImg2ImgPipeline,
     StableDiffusionXLPipeline,
     StableDiffusionXLImg2ImgPipeline,
+    StableDiffusionXLControlNetPipeline,
+)
+
+
+TStableDiffusionPipelineOutput = TypeVar(
+    "TStableDiffusionPipelineOutput",
+    StableDiffusionPipelineOutput,
+    StableDiffusionXLPipelineOutput,
 )
 
 
@@ -36,37 +50,29 @@ class StableDiffusionPipelineEmbedding:
 
 
 @dataclass
-class StableDiffusionPipelineState:
+class StableDiffusionPipelineTaskState:
     """
     Stable diffusion pipeline state information
     """
 
-    step: int
+    step: Optional[int] = None
     progress: Optional[float] = None
-    output: Optional[StableDiffusionPipelineOutput] = None
+    output: Optional[
+        Union[
+            Union[
+                Tuple,
+                StableDiffusionPipelineOutput,
+            ],
+            Union[
+                Tuple,
+                StableDiffusionXLPipelineOutput,
+            ],
+        ]
+    ] = None
 
 
 @dataclass
-class StableDiffusionXLPipelineState:
-    """
-    Stable diffusion xl pipeline state information
-    """
-
-    step: int
-    progress: Optional[float] = None
-    output: Optional[StableDiffusionXLPipelineOutput] = None
-
-
-# Supported stable diffusion pipeline states
-TStableDiffusionPipelineState = TypeVar(
-    "TStableDiffusionPipelineState",
-    StableDiffusionPipelineState,
-    StableDiffusionXLPipelineState,
-)
-
-
-@dataclass
-class StableDiffusionPipelineConfig:
+class StableDiffusionPipelineRunConfig:
     """
     Configuration arguments for stable diffusion pipeline
     """
@@ -92,7 +98,7 @@ class StableDiffusionPipelineConfig:
 
 
 @dataclass
-class StableDiffusionImg2ImgPipelineConfig:
+class StableDiffusionImg2ImgPipelineRunConfig:
     prompt: Optional[Union[str, List[str]]] = None
     image: Optional[
         Union[
@@ -121,7 +127,7 @@ class StableDiffusionImg2ImgPipelineConfig:
 
 
 @dataclass
-class StableDiffusionXLPipelineConfig:
+class StableDiffusionXLPipelineRunConfig:
     """
     Configuration arguments for stable diffusion xl pipeline
     """
@@ -155,7 +161,7 @@ class StableDiffusionXLPipelineConfig:
 
 
 @dataclass
-class StableDiffusionXLImg2ImgPipelineConfig:
+class StableDiffusionXLImg2ImgPipelineRunConfig:
     """
     Configuration arguments for stable diffusion xl img2img pipeline
     """
@@ -172,11 +178,11 @@ class StableDiffusionXLImg2ImgPipelineConfig:
             List[np.ndarray],
         ]
     ] = None
-    strength: float = 0.3
+    strength: float = 0.8
     num_inference_steps: int = 50
     denoising_start: Optional[float] = None
     denoising_end: Optional[float] = None
-    guidance_scale: float = 5.0
+    guidance_scale: float = 8.0
     negative_prompt: Optional[Union[str, List[str]]] = None
     negative_prompt_2: Optional[Union[str, List[str]]] = None
     num_images_per_prompt: Optional[int] = 1
@@ -198,3 +204,59 @@ class StableDiffusionXLImg2ImgPipelineConfig:
     target_size: Optional[Tuple[int, int]] = None
     aesthetic_score: float = 6.0
     negative_aesthetic_score: float = 2.5
+
+
+@dataclass
+class StableDiffusionXLControlNetPipelineRunConfig:
+    """
+    Configuration arguments for stable diffusion xl control net pipeline
+    """
+
+    prompt: Optional[Union[str, List[str]]] = None
+    prompt_2: Optional[Union[str, List[str]]] = None
+    image: Optional[
+        Union[
+            torch.FloatTensor,
+            PIL.Image.Image,
+            np.ndarray,
+            List[torch.FloatTensor],
+            List[PIL.Image.Image],
+            List[np.ndarray],
+        ]
+    ] = None
+    height: Optional[int] = None
+    width: Optional[int] = None
+    num_inference_steps: int = 50
+    guidance_scale: float = 5.0
+    negative_prompt: Optional[Union[str, List[str]]] = None
+    negative_prompt_2: Optional[Union[str, List[str]]] = None
+    num_images_per_prompt: Optional[int] = 1
+    eta: float = 0.0
+    # generator: Optional[Union[torch.Generator, List[torch.Generator]]] = None
+    latents: Optional[torch.FloatTensor] = None
+    prompt_embeds: Optional[torch.FloatTensor] = None
+    negative_prompt_embeds: Optional[torch.FloatTensor] = None
+    pooled_prompt_embeds: Optional[torch.FloatTensor] = None
+    negative_pooled_prompt_embeds: Optional[torch.FloatTensor] = None
+    output_type: Optional[str] = "pil"
+    return_dict: bool = True
+    # callback: Optional[Callable[[int, int, torch.FloatTensor], None]] = None
+    callback_steps: int = 1
+    cross_attention_kwargs: Optional[Dict[str, Any]] = None
+    controlnet_conditioning_scale: Union[float, List[float]] = 0.5
+    guess_mode: bool = False
+    control_guidance_start: Union[float, List[float]] = 0.0
+    control_guidance_end: Union[float, List[float]] = 1.0
+    original_size: Optional[Tuple[int, int]] = None
+    crops_coords_top_left: Tuple[int, int] = (0, 0)
+    target_size: Optional[Tuple[int, int]] = None
+
+
+TStableDiffusionPipelineRunConfig = TypeVar(
+    "TStableDiffusionPipelineRunConfig",
+    StableDiffusionPipelineRunConfig,
+    StableDiffusionImg2ImgPipelineRunConfig,
+    StableDiffusionXLPipelineRunConfig,
+    StableDiffusionXLImg2ImgPipelineRunConfig,
+    StableDiffusionXLControlNetPipelineRunConfig,
+)
